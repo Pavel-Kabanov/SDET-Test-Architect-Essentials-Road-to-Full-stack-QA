6aba09303e7f6de22059c06a8fe17b4db6811d46
JM <pkabanov.d.@gmail.com>
2021-12-18 21:52:07 +0300

    14. Apache common String Utilities to Parse strings with escape characters

--- src.main/java/JsonJava/OneJsonToJava.java
+++ src.main/java/JsonJava/OneJsonToJava.java
@@ -2,10 +2,12 @@ package JsonJava;
 
 import com.google.gson.Gson;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
@@ -32,16 +34,27 @@ public class OneJsonToJava {
             ));
         }
 
-        // Writing to files section.
-        // Clear folder with previous results
-        FileUtils.cleanDirectory(new File("src.main/java/JsonJava/JsonResult"));
-
+        // Adding objects as JSON to an array
         for (CustomerDetails customerDetails : arrayList) {
             jsonArray.add(new Gson().toJson(customerDetails));
         }
 
-        new JSONObject().put("data", jsonArray);
+        // Preparing JSON to a String
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("data", jsonArray);
+        String unescapedString = StringEscapeUtils.unescapeJava(jsonObject.toString());
+        // Fixing problem with extra quotes
+        String unescapedStringQuotesFix = unescapedString
+                .replace("\"{", "{")
+                .replace("}\"", "}");
 
+        // Writing to file section.
+        // Clear folder with previous result
+        FileUtils.cleanDirectory(new File("src.main/java/JsonJava/JsonResult"));
+        // Writing
+        try (FileWriter fileWriter = new FileWriter("src.main/java/JsonJava/JsonResult/transactions.json")) {
+            fileWriter.write(unescapedStringQuotesFix);
+        }
 
         connection.close();
     }
