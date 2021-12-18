2924641a942dfec720c76c37d547dffe6ab509a2
JM <pkabanov.d.@gmail.com>
2021-12-18 15:24:57 +0300

    11. Retrieve Multiple results into Array list of Java objects for further Parsing

--- src.main/java/JsonJava/JsonToJava.java
+++ src.main/java/JsonJava/JsonToJava.java
@@ -1,29 +1,39 @@
 package JsonJava;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class JsonToJava {
     public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+        // DB section
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Business", "JavaAllPermissions", "admin");
         Statement statement = connection.createStatement();
-        ResultSet result = statement.executeQuery("select * from CustomerInfo where Location = 'Asia' limit 1;");
+        ResultSet result = statement.executeQuery("select * from CustomerInfo where Location = 'Asia';");
 
-        CustomerDetails customerDetails = new CustomerDetails();
-
+        ArrayList<CustomerDetails> arrayList = new ArrayList<>();
         while (result.next()) {
-            customerDetails.setCourseName(result.getString(1));
-            customerDetails.setPurchaseDate(result.getString(2));
-            customerDetails.setAmount(result.getInt(3));
-            customerDetails.setLocation(result.getString(4));
+            arrayList.add(new CustomerDetails(
+                    result.getString(1), // courseName
+                    result.getString(2), // purchaseDate
+                    result.getInt(3), // amount
+                    result.getString(4) // location
+            ));
         }
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.writeValue(new File("src.main/java/Jsons/customerInfo.json"), customerDetails);
+        // Writing to files section.
+        // Clear folder with previous results
+        FileUtils.cleanDirectory(new File("src.main/java/JsonJava/Jsons"));
+        // TODO still didn't understand why we can't write to only one file
+        for (int i = 0; i < arrayList.size(); i++) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(new File("src.main/java/JsonJava/Jsons/customerInfo_" + i + ".json"), arrayList.get(i));
+        }
 
         connection.close();
     }
