d2e18a3daf764a0a6e649f904ff0601a27077e95
JM <pkabanov.d.@gmail.com>
2021-12-23 22:41:09 +0300

    28. Create Window Batch files for invoking server

---
+++ src.main/java/DockerValidation/Utils/startDocker.java
@@ -0,0 +1,13 @@
+package DockerValidation.Utils;
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class startDocker {
+    @Test
+    public void startFile() throws IOException {
+        Runtime runtime = Runtime.getRuntime();
+        runtime.exec("cmd /c start dockerUp.bat");
+    }
+}
