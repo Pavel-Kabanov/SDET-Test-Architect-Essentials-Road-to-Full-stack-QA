b0e08546f111a43ae5aab76775e27853ce1bf2ef
JM <pkabanov.d.@gmail.com>
2021-12-24 00:37:06 +0300

    29. Reading the Server log files through Java using Buffered Reader

---
+++ docker/dockerUp.bat
@@ -0,0 +1,3 @@
+@REM set FileName=Build-%date%-%time:~0,2%-%time:~3,2%-%time:~6,2%
+@REM docker-compose -f docker\docker-compose.yml up >> docker\log\%FileName%.log
+docker-compose -f docker\docker-compose.yml up >> docker\log\docker.log
--- src.main/java/DockerValidation/Utils/startDocker.java
+++ src.main/java/DockerValidation/Utils/startDocker.java
@@ -1,13 +1,38 @@
 package DockerValidation.Utils;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class startDocker {
     @Test
-    public void startFile() throws IOException {
+    public void startFile() throws IOException, InterruptedException {
         Runtime runtime = Runtime.getRuntime();
-        runtime.exec("cmd /c start dockerUp.bat");
+        runtime.exec("cmd /c start docker\\dockerUp.bat");
+
+        String dockerLogFile = "docker/log/docker.log";
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(dockerLogFile));
+        String currentLine = bufferedReader.readLine();
+        int counter = 0;
+        boolean flag = false;
+
+        while (currentLine != null) {
+            if (currentLine.contains("from DOWN to UP")) {
+                counter++;
+                System.out.println(counter + " line.");
+            }
+            if (counter == 3) {
+                flag = true;
+                break;
+            }
+            currentLine = bufferedReader.readLine();
+        }
+        bufferedReader.close();
+        Assert.assertTrue(flag);
+        Thread.sleep(3000);
+
     }
 }
