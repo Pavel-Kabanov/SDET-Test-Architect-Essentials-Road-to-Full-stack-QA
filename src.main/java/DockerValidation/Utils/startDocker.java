28c23dfef5afb0e32ab1383ef426250c6ed324c9
JM <pkabanov.d.@gmail.com>
2021-12-25 12:57:01 +0300

    30. Monitor Server logs continuously from Your program for Synchronizing tests

--- src.main/java/DockerValidation/Utils/startDocker.java
+++ src.main/java/DockerValidation/Utils/startDocker.java
@@ -4,8 +4,10 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Calendar;
 
 public class startDocker {
     @Test
@@ -14,23 +16,30 @@ public class startDocker {
         runtime.exec("cmd /c start docker\\dockerUp.bat");
 
         String dockerLogFile = "docker/log/docker.log";
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(dockerLogFile));
-        String currentLine = bufferedReader.readLine();
+        new File(dockerLogFile);
         int counter = 0;
         boolean flag = false;
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.SECOND, 45);
+        long stopNow = calendar.getTimeInMillis();
 
-        while (currentLine != null) {
-            if (currentLine.contains("from DOWN to UP")) {
-                counter++;
-                System.out.println(counter + " line.");
+        while (System.currentTimeMillis() < stopNow) {
+            if (flag) break;
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(dockerLogFile));
+            String currentLine = bufferedReader.readLine();
+            while (currentLine != null && !flag) {
+                if (currentLine.contains("from DOWN to UP")) {
+                    counter++;
+                    System.out.println(counter + " line.");
+                }
+                if (counter == 3) {
+                    flag = true;
+                    break;
+                }
+                currentLine = bufferedReader.readLine();
             }
-            if (counter == 3) {
-                flag = true;
-                break;
-            }
-            currentLine = bufferedReader.readLine();
+            bufferedReader.close();
         }
-        bufferedReader.close();
         Assert.assertTrue(flag);
         Thread.sleep(3000);
 
